8e0e2705782ff8699b92193f4bcffe76e43ed2cf
ldhldh07 <dhlim07@naver.com>
2025-07-23 18:10:22 +0900

    feat: useAutoCallback 정의

--- packages/lib/src/hooks/useAutoCallback.ts
+++ packages/lib/src/hooks/useAutoCallback.ts
@@ -3,5 +3,12 @@ import { useCallback } from "./useCallback";
 import { useRef } from "./useRef";
 
 export const useAutoCallback = <T extends AnyFunction>(fn: T): T => {
-  return fn;
+  const fnRef = useRef<T>(fn);
+  fnRef.current = fn;
+
+  const memoizedCallback = useCallback((...args: Parameters<T>) => {
+    return fnRef.current(...args);
+  }, []);
+
+  return memoizedCallback as T;
 };
