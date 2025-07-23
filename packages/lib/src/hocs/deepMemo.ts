d40bf27e786cce66817b8b6f5f4a2ee2f6febd78
ldhldh07 <dhlim07@naver.com>
2025-07-23 18:36:40 +0900

    feat:deepMemo 정의

--- packages/lib/src/hocs/deepMemo.ts
+++ packages/lib/src/hocs/deepMemo.ts
@@ -1,5 +1,6 @@
-import type { FunctionComponent } from "react";
+import { memo, type FunctionComponent } from "react";
+import { deepEquals } from "../equals";
 
 export function deepMemo<P extends object>(Component: FunctionComponent<P>) {
-  return Component;
+  return memo<P>(Component, deepEquals);
 }
