72a30b9a2d6e0215d76fc3540f5e8c236b37fa86
ldhldh07 <dhlim07@naver.com>
2025-07-23 13:20:22 +0900

    feat: deepEquals 정의

--- packages/lib/src/equals/deepEquals.ts
+++ packages/lib/src/equals/deepEquals.ts
@@ -1,3 +1,18 @@
-export const deepEquals = (a: unknown, b: unknown) => {
-  return a === b;
-};
+export function deepEquals(objA: unknown, objB: unknown): boolean {
+  if (objA === objB) return true;
+
+  if (objA == null || objB == null || typeof objA !== "object" || typeof objB !== "object") return false;
+
+  const keysOfA = Object.keys(objA);
+  const keysOfB = Object.keys(objB);
+
+  if (keysOfA.length !== keysOfB.length) return false;
+
+  for (const key of keysOfA) {
+    const valueA = (objA as Record<string, unknown>)[key];
+    const valueB = (objB as Record<string, unknown>)[key];
+    if (!deepEquals(valueA, valueB)) return false;
+  }
+
+  return true;
+}
