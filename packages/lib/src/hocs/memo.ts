cf2b2399dc28f4b83a27bce6ffdb784b32d822a6
ldhldh07 <dhlim07@naver.com>
2025-07-23 18:36:13 +0900

    feat: memo 정의

--- packages/lib/src/hocs/memo.ts
+++ packages/lib/src/hocs/memo.ts
@@ -1,6 +1,16 @@
-import { type FunctionComponent } from "react";
+import { createElement, type FunctionComponent, type ReactElement } from "react";
 import { shallowEquals } from "../equals";
+import { useRef } from "../hooks";
 
 export function memo<P extends object>(Component: FunctionComponent<P>, equals = shallowEquals) {
-  return Component;
+  return function MemorizedComponent(props: P) {
+    const propsRef = useRef<P | null>(null);
+    const componentRef = useRef<ReactElement | null>(null);
+
+    if (propsRef.current !== null && equals(propsRef.current, props)) return componentRef.current;
+
+    propsRef.current = props;
+    componentRef.current = createElement(Component, props);
+    return componentRef.current;
+  };
 }
