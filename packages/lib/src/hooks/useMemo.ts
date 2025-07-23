d6ed4260329bd99dfc17374872fa02c8d4c2c473
ldhldh07 <dhlim07@naver.com>
2025-07-23 15:04:19 +0900

    feat: useMemo 정의

--- packages/lib/src/hooks/useMemo.ts
+++ packages/lib/src/hooks/useMemo.ts
@@ -1,7 +1,21 @@
 import type { DependencyList } from "react";
 import { shallowEquals } from "../equals";
+import { useRef } from "./useRef";
 
-export function useMemo<T>(factory: () => T, _deps: DependencyList, _equals = shallowEquals): T {
-  // 직접 작성한 useRef를 통해서 만들어보세요.
-  return factory();
+interface MemoCache<T> {
+  deps: DependencyList | undefined;
+  value: T;
+}
+
+export function useMemo<T>(factory: () => T, deps: DependencyList, equals = shallowEquals): T {
+  const ref = useRef<MemoCache<T> | null>(null);
+
+  if (ref.current?.deps && equals(ref.current.deps, deps)) return ref.current.value;
+
+  ref.current = {
+    deps: deps,
+    value: factory(),
+  };
+
+  return ref.current.value;
 }
